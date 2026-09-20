063317976d1deb6a5ca5bf7a32f7a0d77a6d26c2
AayutGit <aayutrohela09@gmail.com>
2026-09-20 17:41:06 +0530

    add .env.production

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://aayut1111.github.io",
+    ],
+  })
+);
 app.use(express.json({ limit: "5mb" }));
 
 const supabase = createClient(
